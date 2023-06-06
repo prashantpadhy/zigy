b46cecf335a8a75e3a410b4b603b1896e376f36d
Prashant Padhy <prashantpadhy21@gmail.com>
2023-06-06 22:23:41 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  
+  const [textboxValues, setTextboxValues] = useState({});
+  const [checkboxValues, setCheckboxValues] = useState({});
+
+  const handleTextboxChange = (e) => {
+    setTextboxValues((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleCheckboxChange = (e) => {
+    setCheckboxValues((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.checked,
+    }));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+
+    <div>
+    <h1>Web Page 1</h1>
+
+    <div>
+      <label htmlFor="textbox1">Textbox 1:</label>
+      <input
+        type="text"
+        id="textbox1"
+        name="textbox1"
+        value={textboxValues['textbox1'] || ''}
+        onChange={handleTextboxChange}
+      />
+      <input
+        type="checkbox"
+        id="checkbox1"
+        name="checkbox1"
+        checked={checkboxValues['checkbox1'] || false}
+        onChange={handleCheckboxChange}
+      />
+      <label htmlFor="checkbox1">Checkbox 1</label>
     </div>
+
+    <div>
+      <h1>Web Page 2</h1>
+
+      <div>
+        <input
+          type="checkbox"
+          id="checkbox1"
+          name="checkbox1"
+          checked={checkboxValues['checkbox1'] || false}
+          onChange={handleCheckboxChange}
+        />
+        <label htmlFor="checkbox1">1. Checkbox 1</label>
+      </div>
+
+      {/* Repeat the above div for each checkbox */}
+
+      <div>
+        {/* Real-time data will be displayed here */}
+        {checkboxValues['checkbox1'] ? (
+          <p>Checkbox 1 is checked.<br/>{textboxValues['textbox1']}</p>
+          
+        ) : (
+          <p>Checkbox 1 is not checked.</p>
+        )}
+        
+        
+      </div>
+    </div>
+
+    {/* Repeat the above div for each additional textbox and checkbox */}
+  </div>
+
+  
   );
 }
 
